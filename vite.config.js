2e97c8d1c703bd8b1fd243f0803b85664bfde684
nizam-321 <bhainizam303@gmail.com>
2025-04-16 18:45:15 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Login-Signup-Form-with-Validation/', // 👈 apne repo ka naam daalo
   plugins: [react()],
 })
